6720cafaf081de31fcd1924bdcd8e1bf3f5db61f
LiKunlong <lkl0801@sina.cn>
2021-08-30 14:34:26 +0800

    提交

---
+++ bit_class-master/bit_class-master/java17blog/src/main/java/service/HelloServlet.java
@@ -0,0 +1,23 @@
+package service;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hi")
+public class HelloServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doGet(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setCharacterEncoding("utf-8");
+        response.setContentType("text/html");
+        PrintWriter writer = response.getWriter();
+        writer.println("<h1>你好~</h1>");
+    }
+}
